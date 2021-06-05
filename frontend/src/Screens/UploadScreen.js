59bc6b700151759090d00116e5198e7199fd2e73
Raghav Ranjan <ranjan.1@iitj.ac.in>
2021-06-05 15:21:49 +0530

    updated upload page

--- frontend/src/Screens/UploadScreen.js
+++ frontend/src/Screens/UploadScreen.js
@@ -14,13 +14,13 @@ function UploadScreen(props) {
             <h1 className="large"><i className="fa fa-upload"></i> Upload Video</h1>
         </div>
 
+        <div class="card-box">
 
-        <form>
-            
+          <img className="upload-cartoon" src="https://cartoonhub.s3.ap-south-1.amazonaws.com/upload-video.png"></img>
+          <p className="card-box-p">Create, Upload and publish a video<br></br> to get started.</p>
 
-            <input className="upload-box"></input>
-
-        </form>
+          <button class="open-button" >Upload Video</button>
+        </div>
        
         </BrowserRouter>
     
